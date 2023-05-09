1515c0ea082f56eab128f1d4deb631f29bf7477a
Nikos Chantziaras <realnc@gmail.com>
2023-05-10 01:45:52 +0300

    fluidsynth: Update to 2.3.2

--- src/fluidsynth.mk
+++ src/fluidsynth.mk
@@ -4,21 +4,36 @@ PKG             := fluidsynth
 $(PKG)_WEBSITE  := http://fluidsynth.org/
 $(PKG)_DESCR    := FluidSynth - a free software synthesizer based on the SoundFont 2 specifications
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.7
-$(PKG)_CHECKSUM := b68876d24c7fb34575ffa389bcfe8e61a24f1cf1da8ec6c3b2053efde98d0320
+$(PKG)_VERSION  := 2.3.2
+$(PKG)_CHECKSUM := cd610810f30566e28fb98c36501f00446a06fa6bae3dc562c8cd3868fe1c0fc7
 $(PKG)_GH_CONF  := FluidSynth/fluidsynth/tags,v
-$(PKG)_DEPS     := cc dbus glib jack libsndfile mman-win32 portaudio readline
+$(PKG)_DEPS     := cc glib mman-win32
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' '$(SOURCE_DIR)' \
         -Dbuild-bins=OFF \
         -Dbuild-docs=OFF \
         -Dbuild-tests=OFF \
-        -Denable-dbus=ON \
-        -Denable-jack=$(CMAKE SHARED_BOOL) \
-        -Denable-libsndfile=ON \
-        -Denable-portaudio=ON \
-        -Denable-readline=ON \
+        -Denable-dbus=OFF \
+        -Denable-jack=OFF \
+        -Denable-libsndfile=OFF \
+        -Denable-portaudio=OFF \
+        -Denable-readline=OFF \
+        -Denable-aufile=OFF \
+        -Denable-ipv6=OFF \
+        -Denable-ladspa=OFF \
+        -Denable-libinstpatch=OFF \
+        -Denable-midishare=OFF \
+        -Denable-opensles=OFF \
+        -Denable-network=OFF \
+        -Denable-dsound=OFF \
+        -Denable-oss=OFF \
+        -Denable-wasapi=OFF \
+        -Denable-waveout=OFF \
+        -Denable-winmidi=OFF \
+        -Denable-sdl2=OFF \
+        -Denable-pulseaudio=OFF \
+        -Denable-threads=ON \
         $($(PKG)_CONFIGURE_OPTS)
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
@@ -28,5 +43,5 @@ define $(PKG)_BUILD
         -W -Wall -Werror -ansi -pedantic \
         -Wl,--allow-multiple-definition \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-fluidsynth.exe' \
-        `'$(TARGET)-pkg-config' --cflags --libs fluidsynth`
+        `'$(TARGET)-pkg-config' --cflags --libs fluidsynth` -luuid
 endef
