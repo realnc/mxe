be3722a9813722b1fc55cdab0c2e7bbd53163d6e
Nikos Chantziaras <realnc@gmail.com>
2023-05-10 01:49:21 +0300

    libsndfile: Update to 1.2.0, disable external libs and testsuite

--- src/libsndfile.mk
+++ src/libsndfile.mk
@@ -1,20 +1,23 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libsndfile
-$(PKG)_VERSION  := 1.1.0
-$(PKG)_CHECKSUM := 0f98e101c0f7c850a71225fb5feaf33b106227b3d331333ddc9bacee190bcf41
-$(PKG)_GH_CONF  := libsndfile/libsndfile/releases/latest,,,,,.tar.xz
-$(PKG)_DEPS     := cc flac ogg vorbis opus
+$(PKG)_VERSION  := 1.2.0
+$(PKG)_FILE     := libsndfile-$($(PKG)_VERSION).tar.xz
+$(PKG)_CHECKSUM := 0e30e7072f83dc84863e2e55f299175c7e04a5902ae79cfb99d4249ee8f6d60a
+$(PKG)_SUBDIR   := libsndfile-$($(PKG)_VERSION)
+$(PKG)_URL      := https://github.com/libsndfile/libsndfile/releases/download/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc
 
 define $(PKG)_BUILD
     cd '$(SOURCE_DIR)' && autoreconf -fi
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
         $(MXE_CONFIGURE_OPTS) \
         --disable-sqlite \
-        --enable-external-libs \
         --disable-octave \
         --disable-alsa \
         --disable-shave \
+        --disable-external-libs \
+        --disable-full-suite \
         PKG_CONFIG='$(TARGET)-pkg-config'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' $(MXE_DISABLE_CRUFT)
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install $(MXE_DISABLE_CRUFT)
